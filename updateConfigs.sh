ee0a8777ba78cd0a1cd66a63a5cd93ebedb06aa9
Vladgobelen <vladgobelen@gmail.com>
2026-05-25 00:43:57 +0700

    Пн 25 мая 2026 00:43:57 +07

--- updateConfigs.sh
+++ updateConfigs.sh
@@ -2,7 +2,7 @@
 cd "/home/diver/sources/JS/NSLorPanel/"
 j=$(date)
 git add .
-git commit -m "Закрытие настроек по ESC. Сохранение скролла при показанных удаленных сообщениях и не только."
+git commit -m "$1 $j"
 git push git@github.com:Vladgobelen/NSLorPanel.git
 git add .
 git commit -m "$1 $j"
