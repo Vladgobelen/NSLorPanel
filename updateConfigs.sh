2b51c22bb5cd1c3be709c3b9c1c98e035e7d768c
Vladgobelen <vladgobelen@gmail.com>
2026-05-24 14:54:48 +0700

    Реализация визуальной панели. Очень кривая тестовая. Итого - два редактора.

--- updateConfigs.sh
+++ updateConfigs.sh
@@ -2,9 +2,9 @@
 cd "/home/diver/sources/JS/NSLorPanel/"
 j=$(date)
 git add .
-git commit -m "$1 $j"
+git commit -m "Реализация визуальной панели. Очень кривая тестовая. Итого - два редактора."
 git push git@github.com:Vladgobelen/NSLorPanel.git
 git add .
-git commit -m "Вторая реализация редактора: визуальный редактор. Очень криво."
+git commit -m "$1 $j"
 git push
 
