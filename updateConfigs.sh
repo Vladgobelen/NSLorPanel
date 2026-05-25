4a057141d4c37872bbbf02ccd999e9b1cce2484a
Vladgobelen <vladgobelen@gmail.com>
2026-05-26 03:21:22 +0700

    Исправлен скролл из уведомлений, исправлены модалки. Свайп для мобилок вправо.

--- updateConfigs.sh
+++ updateConfigs.sh
@@ -2,7 +2,7 @@
 cd "/home/diver/sources/JS/NSLorPanel/"
 j=$(date)
 git add .
-git commit -m "Покажз комментария на уведомления. На сранице уведомлений и в модалке."
+git commit -m "Исправлен скролл из уведомлений, исправлены модалки. Свайп для мобилок вправо."
 git push git@github.com:Vladgobelen/NSLorPanel.git
 git add .
 git commit -m "$1 $j"
