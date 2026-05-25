9914256d809c11a25c57edecb9fe084d11e2b55d
Vladgobelen <vladgobelen@gmail.com>
2026-05-25 22:34:25 +0700

    Покажз комментария на уведомления. На сранице уведомлений и в модалке.

--- updateConfigs.sh
+++ updateConfigs.sh
@@ -2,7 +2,7 @@
 cd "/home/diver/sources/JS/NSLorPanel/"
 j=$(date)
 git add .
-git commit -m "Исправление скролла к уведомлениям. Раньше переходил вместо уведомлений в сохраненную позицию."
+git commit -m "Покажз комментария на уведомления. На сранице уведомлений и в модалке."
 git push git@github.com:Vladgobelen/NSLorPanel.git
 git add .
 git commit -m "$1 $j"
