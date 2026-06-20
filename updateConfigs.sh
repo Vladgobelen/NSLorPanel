5ac36004ba653c77abc4aba2e5e06bb086407dfa
Vladgobelen <vladgobelen@gmail.com>
2026-06-20 23:26:51 +0700

    Добавил предпросмотр новый в редактор текста

--- updateConfigs.sh
+++ updateConfigs.sh
@@ -2,7 +2,7 @@
 cd "/home/diver/sources/JS/NSLorPanel/"
 j=$(date)
 git add .
-git commit -m "Исправление универсального свайпа мышью."
+git commit -m "Добавил предпросмотр новый в редактор текста"
 git push git@github.com:Vladgobelen/NSLorPanel.git
 git add .
 git commit -m "$1 $j"
